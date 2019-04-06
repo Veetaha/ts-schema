b72ffde0bc6fe0c4fefde28f3062cf18c4316928
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-06 17:02:18 +0000

    Deploy Veetaha/ts-schema-validator to github.com/Veetaha/ts-schema-validator.git:gh-pages

---
+++ assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"1":"External module","65536":"Type literal"},"rows":[{"id":0,"kind":65536,"name":"__type","url":"modules/_types_schema_.html#schemaobj.__type","classes":"tsd-kind-type-literal tsd-parent-kind-type-alias tsd-is-not-exported","parent":"\"types/schema\".SchemaObj"},{"id":1,"kind":65536,"name":"__type","url":"modules/_types_schema_.html#schemapredicate.__type-1","classes":"tsd-kind-type-literal tsd-parent-kind-type-alias tsd-is-not-exported","parent":"\"types/schema\".SchemaPredicate"},{"id":2,"kind":1,"name":"\"index\"","url":"modules/_index_.html","classes":"tsd-kind-external-module"}]};
